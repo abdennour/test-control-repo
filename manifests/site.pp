bdbce35ae22f361da9dd6d9826c14df6bad7f4d1
abdennour <github@abdennoor.com>
2018-06-13 16:26:39 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,9 +7,9 @@ node default {
   
 }
 
-# node 'master.puppet.vm' {
-#  include role::master_server
-# }
+node 'master.puppet.vm' {
+  include role::master_server
+}
 
 node /^web/ {
   include role::app_server
