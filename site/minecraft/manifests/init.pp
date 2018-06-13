3924c5087d463e54540607e2b80db19ec873eb39
abdennour <github@abdennoor.com>
2018-06-13 22:36:44 +0300

    Update init.pp
    Replace "files/minecraft.service" by a compiled template "templates/minecraft.service.epp"

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -26,7 +26,9 @@ class minecraft (
   
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    content => epp('minecraft/minecraft.service', { # check "site/minecraft/templates/minecraft.service.epp"
+      install_dir => $install_dir
+    }),
   }
   
   service {'minecraft':
